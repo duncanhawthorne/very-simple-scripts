d21d46e848671b70412a190b28e382511597d1ab
duncan.hawthorne <duncan.hawthorne@ddb9ca1a-dfec-11dd-84fc-932898d6254d>
2009-04-30 15:05:05 +0000

    automatic repository sync

--- apt_as_filesystem.py
+++ apt_as_filesystem.py
@@ -15,11 +15,17 @@ except ImportError:
 import fuse
 from fuse import Fuse
 
+import apt
+apt_cache = apt.Cache()
+
 def bash(command):		
 	return os.popen(command).read().split("\n")[:-1]
 
 def log(arg):
-	bash("echo "+arg+" >> ~/desktop/log")
+	try:
+		bash("echo "+arg+" >> ~/desktop/log")
+	except:
+		None
 
 installed_apps = []
 a = bash("dpkg --get-selections")
@@ -33,7 +39,7 @@ if not hasattr(fuse, '__version__'):
 
 fuse.fuse_python_api = (0, 2)
 
-hello_path = installed_apps[:5]
+hello_path = installed_apps#[:5]
 hello_str = 'Hello World!\n'
 
 class MyStat(fuse.Stat):
@@ -49,12 +55,21 @@ class MyStat(fuse.Stat):
         self.st_mtime = 0
         self.st_ctime = 0
 
+def is_linked(path):
+	if "/"+path.split("/")[1] in hello_path and len(path.split("/")) >= 4:
+		return True
+	else:
+		return False
+		
+def is_managed_app(path):
+	if "/"+path.split("/")[1] in hello_path:
+		return True
+	else:
+		return False			
+
 def get_target_file_path(path):
 	application = path[1:].split("/")[0]
-	relative_path = path[1:][len(application):]
-	if relative_path == "":
-		relative_path = "/"
-	#log("relative_path"+relative_path)
+	relative_path = path[1:][len(application):][len("/"):][len("bob"):]
 	return relative_path
 
 class HelloFS(Fuse):
@@ -64,23 +79,44 @@ class HelloFS(Fuse):
 		#log("path "+path)
 		st = MyStat()
 		if path == '/':
-			st.st_mode = stat.S_IFDIR | 0755
+			#st.st_mode = stat.S_IFDIR | 0755
+			st.st_mode = stat.S_IFDIR | 0777
 			st.st_nlink = 2
+			st.st_uid = os.getuid()
+			st.st_gid = os.getuid()
 		elif path in hello_path:
 			st.st_mode = stat.S_IFDIR | 0755
-			st.st_nlink = 2 
+			st.st_nlink = 2
+			st.st_uid = os.getuid()
+			st.st_gid = os.getuid()
+		elif is_linked(path):
+			target = get_target_file_path(path)
+			target_stat = os.stat(target)
+			
+			st = target_stat
+			
+	#		st.st_uid = target_stat.st_uid
+	#		st.st_gid = target_stat.st_gid
+	#		st.st_size = target_stat.st_size
+	#		
+	#		#log("in the else bit")
+	#		if os.path.isdir(get_target_file_path(path)):
+	#			#log("dir")
+	#			st.st_mode = stat.S_IFDIR | 0755
+	#			st.st_nlink = 2
+	#			
+	#			
+	#		else:
+	#			#log("file")
+	#			st.st_mode = stat.S_IFREG | 0777
+	#			st.st_nlink = 1
+						
 		else:
-			#log("in the else bit")
-			if os.path.isdir(get_target_file_path(path)):
-				#log("dir")
-				st.st_mode = stat.S_IFDIR | 0755
-				st.st_nlink = 2
-			else:
-				#log("file")
-				st.st_mode = stat.S_IFREG | 0444
-				st.st_nlink = 1
-				
-				st.st_size = len(open(get_target_file_path(path), 'rb').read())#len(hello_str)        			        			
+			st.st_mode = stat.S_IFDIR | 0755
+			st.st_nlink = 2
+			st.st_uid = os.getuid()
+			st.st_gid = os.getuid()					
+				        			        			
 #        elif not path in hello_path:
  #           st.st_mode = stat.S_IFREG | 0444
   #          st.st_nlink = 1
@@ -96,28 +132,39 @@ class HelloFS(Fuse):
 			if path == "/":
 				for r in  ['.', '..'] + list(item[1:] for item in hello_path):
 					yield fuse.Direntry(r)
+			elif is_managed_app(path):
+			
+				if len(path.split("/")) >= 3 and path.split("/")[2] == "bob":
+					application = path[1:].split("/")[0]
+					relative_path = get_target_file_path(path)
+					#log(relative_path)
+					file_list = (str(item) for item in apt_cache[application].installedFiles)#bash("dpkg -L "+application) #should cache this
+					#print file_list
+					#print "rel_path", relative_path
+					files = []
+					for item in file_list:
+						if item[:len(relative_path)] == relative_path: #same start bit
+							filename_to_show = item[len(relative_path)+1:].split("/")[0]
+							if not filename_to_show in files and not filename_to_show == "": #to stop dupes, as there will be loads
+								#log("filename_to_show "+filename_to_show)
+								files.append(filename_to_show) #the bit before the slash directly after the relative path
+					#print files
+					for r in  ['.', '..'] + files:
+						yield fuse.Direntry(r)
+				else:
+					for r in  ['.', '..'] + ["bob"]:
+						yield fuse.Direntry(r)					
 			else:
-				application = path[1:].split("/")[0]
-				#log(application)
-				relative_path = path[1:][len(application):]
-				#log(relative_path)
-				file_list = bash("dpkg -L "+application) #should cache this
-				files = []
-				for item in file_list:
-					if item[:len(relative_path)] == relative_path: #same start bit
-						filename_to_show = item[len(relative_path)+1:].split("/")[0]
-						if not filename_to_show in files and not filename_to_show == "": #to stop dupes, as there will be loads
-							#log("filename_to_show "+filename_to_show)
-							files.append(filename_to_show) #the bit before the slash directly after the relative path
-				for r in  ['.', '..'] + files:
+				for r in  ['.', '..']:
 					yield fuse.Direntry(r)
 
 	def open(self, path, flags):
-		if False:#not path in hello_path:
-			return -errno.ENOENT
-		accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
-		if (flags & accmode) != os.O_RDONLY:
-			return -errno.EACCES
+		return
+	#	if False:#not path in hello_path:
+	#		return -errno.ENOENT
+	#	accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
+	#	if (flags & accmode) != os.O_RDONLY:
+	#		return -errno.EACCES
 
 	def read(self, path, size, offset):
 		if False:#not path in hello_path:
@@ -125,18 +172,70 @@ class HelloFS(Fuse):
 		f = open(get_target_file_path(path), 'rb').read()
 		content = f #previously content = hello_str
 		#content = encode(content)
-		log(content)
+		#log(content)
 	
 		
 		slen = len(content)
 		if offset < slen:
 			if offset + size > slen:
 				size = slen - offset
-				buf = content[offset:offset+size]
-			else:
-				buf = ''
+			buf = content[offset:offset+size]
+		else:
+			buf = ''
 		return buf
 
+	#doenst work from here down
+	
+	def write(self, path, buf, offset, fh=None):
+		"""
+		Writes to the file.
+		Returns the number of bytes written.
+		"""
+		f = open(get_target_file_path(path), 'r').read()
+		content = f #previously content = hello_str		
+		
+		
+		if offset < len(content):
+			# Write over part of the file. Save the bits we want to keep.
+			before = content[:offset]
+			after = content[offset+len(buf):]
+		else:
+			if offset > len(content):
+				# First pad the file with 0s, using truncate
+				content = content + '\0'*(size-len(content))
+			before = content
+			after = ''
+			
+		# Insert buf in between before and after
+		new_content = before + buf + after
+		
+		#log(new_content)
+		
+		#f.close()
+		
+		g = open(get_target_file_path(path), 'w')
+		g.write(new_content)
+		g.close()
+		return len(buf)		
+
+	def ftruncate(self, path, size, fh=None):
+		return -errno.EOPNOTSUPP#
+		
+	def create(self, path, mode, rdev):
+		return -errno.EOPNOTSUPP
+
+	def fgetattr(self, path, fh=None):
+		return -errno.EOPNOTSUPP
+
+	def release(self, path, flags, fh=None):
+		None
+
+	def fsync(self, path, datasync, fh=None):
+		None
+
+	def flush(self, path, fh=None):	
+		None
+		
 def main():
     usage="""
 Userspace hello example
