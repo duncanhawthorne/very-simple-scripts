5e1b719c50c1ee1795d265f7dec68d331ebe8be8
duncan.hawthorne <duncan.hawthorne@ddb9ca1a-dfec-11dd-84fc-932898d6254d>
2009-05-02 16:53:52 +0000

    automatic repository sync

--- cached.py
+++ cached.py
@@ -7,7 +7,9 @@ import sys
 sys.setrecursionlimit(200000)
 
 cached_results  = {}
+
 def cached(function, args):
+	#print("running cached")
 	assert type(args) == tuple, "you need to pass a tuple, if you want to pass one arg, pass (arg,)"
 	if function in cached_results and args in cached_results[function]:
 		return cached_results[function][args]
@@ -17,9 +19,53 @@ def cached(function, args):
 			cached_results[function] = {}
 		cached_results[function][args] = answer
 		return answer
+
+decode_list = {"current":None, "orig":None}
+		
+def bob(args):
+	#print "bob", args
+	return cached(decode_list["orig"], (args,))
+		
+def cached_seamless(function, args):
+	'''
+	This is not threadsafe
+	'''
+	#print function, args
+	
+	
+	orig_function = function
+	
+	
+	
+	#print "cacehd_seamless", function, args
+	decode_list["orig"] = function
+	
+	#print locals()
+	
+	for item in globals():
+		if not item == "bob":
+			if globals()[item] == function:
+				external_function = item
+	
+	exec external_function+" = bob" in globals()
+	
+	answer =  cached(bob, (n,))
+	
+	exec external_function+" = decode_list['orig']" in globals()
+	#now reset the function back again
+	
+	#TODO
+	
+	return answer
+	
+
+	
+	
+	
 		
 	
 def fib_slow(n):
+	#print("running fib_slow")
 	if n == 1 or n == 2:
 		return 1
 	else:
@@ -54,7 +100,19 @@ def fib_with_cache(n):
 	
 
 if __name__ == "__main__":
-	n = 35
+	n = 70
+	
+	#print globals()
+	
+	print cached_seamless(fib_slow, (10,))
+	print cached_seamless(fib_slow, (n,))
+	
+	print fib_slow(n)
+	
+	#print cached(bob, (n,))
+	quit()
+	
+	
 	print(fib_fast(n))
 	print(fib_with_cache(n))
 	print(fib_slow(n))
