decb4d92b01b771fcede8b60b0962300a4cf1388
duncan.hawthorne <duncan.hawthorne@ddb9ca1a-dfec-11dd-84fc-932898d6254d>
2009-06-09 09:03:34 +0000

    automatic repository sync

--- apt_as_filesystem.py
+++ apt_as_filesystem.py
@@ -2,6 +2,7 @@ import os, stat, errno
 
 import fuse
 from fuse import Fuse
+fuse.fuse_python_api = (0, 2)
 
 import apt
 apt_cache = apt.Cache()
@@ -9,22 +10,11 @@ apt_cache = apt.Cache()
 def bash(command):		
 	return os.popen(command).read().split("\n")[:-1]
 
-def log(arg):
-	try:
-		bash("echo "+arg+" >> ~/desktop/log")
-	except:
-		None
-
-installed_apps = []
-a = bash("dpkg --get-selections")
+app_list = []
+a = bash("dpkg --get-selections")#FIXME do in python
 for item in a:
     if not "deinstall" in item:
-        installed_apps.append("/"+item.split()[0])
-
-fuse.fuse_python_api = (0, 2)
-
-hello_path = installed_apps#[:5]
-hello_str = 'Hello World!\n'
+        app_list.append(item.split()[0])
 
 class MyStat(fuse.Stat):
     def __init__(self):
@@ -39,22 +29,88 @@ class MyStat(fuse.Stat):
         self.st_mtime = 0
         self.st_ctime = 0
 
-def is_linked(path):
-	if "/"+path.split("/")[1] in hello_path and len(path.split("/")) >= 4:
-		return True
+def is_linked_path(path_list):
+	#print("is linked",path_list)
+	if len(path_list) >= 1 and path_list[0] == 'programs':
+		if len(path_list) >= 3 and path_list[2] == 'files':
+			if len(path_list) >= 4:
+				return True
+		if len(path_list) >= 3 and path_list[2] == 'executable':
+			return True
+		if len(path_list) >= 3 and path_list[2] == 'config':
+			if len(path_list) == 4 and not (path_list[-1] == '.' or path_list[-1] == '..'): #ie not /etc itself
+				return True			
+	elif len(path_list) >= 1 and path_list[0] == 'users':
+		if len(path_list) >= 2:
+			return True
+	elif len(path_list) >= 1 and path_list[0] == 'mount':
+		if len(path_list) >= 2:
+			return True
+	elif False:#other linked apps
+		None
 	else:
 		return False
 		
-def is_managed_app(path):
-	if "/"+path.split("/")[1] in hello_path:
-		return True
-	else:
-		return False			
+def get_target_file_path(path_list):
+	assert is_linked_path(path_list)
+	#programs, app_name, files, start of file path
+	if len(path_list) >= 1 and path_list[0] == 'programs':
+		if len(path_list) >= 3 and path_list[2] == 'files':
+			return path_list[3:]
+		elif len(path_list) >= 3 and path_list[2] == 'executable':
+			target = bash('which '+path_list[1])#find location of executable with same name as package
+			assert target != []
+			return path_to_list(target[0])
+		elif len(path_list) >= 3 and path_list[2] == 'config':
+			assert len(path_list) == 4
+			application = path_list[1]
+			installed_files = (str(item) for item in apt_cache[application].installedFiles)#should cache
 
-def get_target_file_path(path):
-	application = path[1:].split("/")[0]
-	relative_path = path[1:][len(application):][len("/"):][len("bob"):]
-	return relative_path
+			for item in installed_files:
+				std_item = path_to_list(item)
+				if len(std_item) >= 2 and std_item[0] == 'etc' and not os.path.isdir(list_to_path(std_item)):#or more... 
+					#2 not 1 is intentional, dont want /etc itself
+					if std_item[-1] == path_list[-1]:
+						return std_item
+			print "we shouldnt have got here", path_list						
+	elif len(path_list) >= 1 and path_list[0] == 'users':
+		return ['home']+path_list[1:]#so just translates users into home
+	elif len(path_list) >= 1 and path_list[0] == 'mount':
+		return ['media']+path_list[1:]#so just translates users into home
+	elif False:#other link paths
+		None
+	else:
+		raise "error"
+		
+def is_fake_file(path_list):
+	#print path_list
+	if len(path_list) >= 1 and path_list[0] == 'programs':
+	#path[1] will be the program
+		if len(path_list) >= 3 and path_list[2] == 'desktop file': 
+			return True		
+	#else
+	return False
+
+def get_fake_file_contents(path_list):
+	return "hello"
+
+def list_to_path(path_list):
+	string = ''
+	for item in path_list:#FIXME what about if no items, shoud it return '/'
+		string = string+'/'+item
+	return string
+
+def path_to_list(path):
+	first = path.split("/")
+	if len(first) == 0:
+		return first
+	if first[0] == '':
+		first = first[1:]
+	if len(first) == 0:
+		return first		
+	if first[-1] == '':
+		first = first[:-1]
+	return first
 
 class HelloFS(Fuse):
 
@@ -62,103 +118,114 @@ class HelloFS(Fuse):
 	def getattr(self, path):
 		#log("path "+path)
 		st = MyStat()
-		if path == '/':
-			#st.st_mode = stat.S_IFDIR | 0755
-			st.st_mode = stat.S_IFDIR | 0777
-			st.st_nlink = 2
+		if is_linked_path(path_to_list(path)):
+			target = get_target_file_path(path_to_list(path))
+			st = os.stat(list_to_path(target))
+		elif is_fake_file(path_to_list(path)):
+			st.st_mode = stat.S_IFREG | 0755
+			st.st_nlink = 1#FIXME does this need to be 1?
 			st.st_uid = os.getuid()
 			st.st_gid = os.getuid()
-		elif path in hello_path:
-			st.st_mode = stat.S_IFDIR | 0755
-			st.st_nlink = 2
-			st.st_uid = os.getuid()
-			st.st_gid = os.getuid()
-		elif is_linked(path):
-			target = get_target_file_path(path)
-			target_stat = os.stat(target)
-			
-			st = target_stat
-			
-	#		st.st_uid = target_stat.st_uid
-	#		st.st_gid = target_stat.st_gid
-	#		st.st_size = target_stat.st_size
-	#		
-	#		#log("in the else bit")
-	#		if os.path.isdir(get_target_file_path(path)):
-	#			#log("dir")
-	#			st.st_mode = stat.S_IFDIR | 0755
-	#			st.st_nlink = 2
-	#			
-	#			
-	#		else:
-	#			#log("file")
-	#			st.st_mode = stat.S_IFREG | 0777
-	#			st.st_nlink = 1
+			st.st_size = len(get_fake_file_contents(path_to_list(path)))#len(whatever it is)
+		elif False:
+			None
+			#pther options, like files i will create from nothing
 						
-		else:
+		else: #default to it being a folder
 			st.st_mode = stat.S_IFDIR | 0755
 			st.st_nlink = 2
 			st.st_uid = os.getuid()
 			st.st_gid = os.getuid()					
 				        			        			
-#        elif not path in hello_path:
- #           st.st_mode = stat.S_IFREG | 0444
-  #          st.st_nlink = 1
-   #         st.st_size = len(hello_str)
-#		else:#if "/"+path.split("/")[1] in hello_path:
-#			st.st_mode = stat.S_IFDIR | 0755
-#			st.st_nlink = 2            
-#        else:
- #           return -errno.ENOENT
 		return st
 
 	def readdir(self, path, offset):
-			if path == "/":
-				for r in  ['.', '..'] + list(item[1:] for item in hello_path):
-					yield fuse.Direntry(r)
-			elif is_managed_app(path):
-			
-				if len(path.split("/")) >= 3 and path.split("/")[2] == "bob":
-					application = path[1:].split("/")[0]
-					relative_path = get_target_file_path(path)
-					#log(relative_path)
-					file_list = (str(item) for item in apt_cache[application].installedFiles)#bash("dpkg -L "+application) #should cache this
-					#print file_list
-					#print "rel_path", relative_path
+		files = []
+		if len(path_to_list(path)) == 0:
+			files = ['programs', 'users', 'system', 'mount']
+		elif path_to_list(path)[0] == 'programs':
+			if len(path_to_list(path)) == 1:
+				files = app_list
+			else: #1 level down, inside program folder
+				#inside program folders
+				application = path_to_list(path)[1]
+				if len(path_to_list(path)) == 2:
+					#top level folder stuff
+					files = ['files', 'config', 'data']
+					tmp = bash('which '+(path_to_list(path))[1])
+					if not tmp == []:#ie this package has an associated executable
+						files += ['executable', 'desktop file']
+				elif path_to_list(path)[2] == 'files':
+					installed_files = (str(item) for item in apt_cache[application].installedFiles)#should cache
+					#bash("dpkg -L "+application) 
+					#print installed_files
 					files = []
-					for item in file_list:
-						if item[:len(relative_path)] == relative_path: #same start bit
-							filename_to_show = item[len(relative_path)+1:].split("/")[0]
-							if not filename_to_show in files and not filename_to_show == "": #to stop dupes, as there will be loads
-								#log("filename_to_show "+filename_to_show)
-								files.append(filename_to_show) #the bit before the slash directly after the relative path
-					#print files
-					for r in  ['.', '..'] + files:
-						yield fuse.Direntry(r)
+					for item in installed_files:
+						std_item = path_to_list(item)
+						if (['programs', application, 'files']+std_item)[:-1] == path_to_list(path):
+							files.append(std_item[-1])
+				elif path_to_list(path)[2] == 'config':
+					assert len(path_to_list(path)) <= 4 #just want flat config structure #FIXME files with the same name in different folders
+					installed_files = (str(item) for item in apt_cache[application].installedFiles)#should cache
+					files = []
+					for item in installed_files:
+						std_item = path_to_list(item)
+						if len(std_item) >= 2 and std_item[0] == 'etc' and not os.path.isdir(list_to_path(std_item)):#or more... 
+							#2 not 1 is intentional, dont want /etc itself
+							files.append(std_item[-1])
+				elif path_to_list(path)[2] == 'data':
+					None				
 				else:
-					for r in  ['.', '..'] + ["bob"]:
-						yield fuse.Direntry(r)					
-			else:
-				for r in  ['.', '..']:
-					yield fuse.Direntry(r)
-
+					print "readir else", path
+					raise "error"
+					#shouldnt get to an else statement
+					
+		elif path_to_list(path)[0] == 'users':
+			files = bash('ls /home'+list_to_path((path_to_list(path))[1:]))
+		elif path_to_list(path)[0] == 'system':
+			None
+		elif path_to_list(path)[0] == 'mount':
+			files = bash('ls /media'+list_to_path((path_to_list(path))[1:]))
+			#files += bash('ls ~/.gvfs')#+list_to_path((path_to_list(path))[1:]))
+		elif path_to_list(path)[0] == '.Trash-1000':
+			None	
+		elif path_to_list(path)[0] == '.Trash':
+			None						
+		else:
+			print "readir else", path
+			raise "error"
+		
+		#the yield that everything uses
+		for r in  ['.', '..'] + files:
+			yield fuse.Direntry(r)
+						
+			
 	def open(self, path, flags):
 		return
-	#	if False:#not path in hello_path:
+	#	if False:#not path in app_list:
 	#		return -errno.ENOENT
 	#	accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
 	#	if (flags & accmode) != os.O_RDONLY:
 	#		return -errno.EACCES
 
 	def read(self, path, size, offset):
-		if False:#not path in hello_path:
+		if False:#not path in app_list:
 			return -errno.ENOENT
-		f = open(get_target_file_path(path), 'rb').read()
-		content = f #previously content = hello_str
-		#content = encode(content)
-		#log(content)
-	
+		if is_linked_path(path_to_list(path)):
+			content = open(list_to_path(get_target_file_path(path_to_list(path))), 'rb').read()
+
+		elif is_fake_file(path_to_list(path)):
+			content = get_fake_file_contents(path_to_list(path))
+			#need to work out the buffer thing
+			
 		
+		elif False:
+			#files created from nothing 
+			None
+		else:
+			raise "error"
+			
+		#the main return that everything uses	
 		slen = len(content)
 		if offset < slen:
 			if offset + size > slen:
@@ -166,7 +233,7 @@ class HelloFS(Fuse):
 			buf = content[offset:offset+size]
 		else:
 			buf = ''
-		return buf
+		return buf	
 
 	#doenst work from here down
 	
@@ -175,32 +242,38 @@ class HelloFS(Fuse):
 		Writes to the file.
 		Returns the number of bytes written.
 		"""
-		f = open(get_target_file_path(path), 'r').read()
-		content = f #previously content = hello_str		
+		if is_linked_path(path_to_list(path)):
+			f = open(get_target_file_path(path), 'r').read()
+			content = f	
 		
 		
-		if offset < len(content):
-			# Write over part of the file. Save the bits we want to keep.
-			before = content[:offset]
-			after = content[offset+len(buf):]
-		else:
-			if offset > len(content):
-				# First pad the file with 0s, using truncate
-				content = content + '\0'*(size-len(content))
-			before = content
-			after = ''
+			if offset < len(content):
+				# Write over part of the file. Save the bits we want to keep.
+				before = content[:offset]
+				after = content[offset+len(buf):]
+			else:
+				if offset > len(content):
+					# First pad the file with 0s, using truncate
+					content = content + '\0'*(size-len(content))
+				before = content
+				after = ''
 			
-		# Insert buf in between before and after
-		new_content = before + buf + after
+			# Insert buf in between before and after
+			new_content = before + buf + after
 		
-		#log(new_content)
+			#log(new_content)
 		
-		#f.close()
+			#f.close()
 		
-		g = open(get_target_file_path(path), 'w')
-		g.write(new_content)
-		g.close()
-		return len(buf)		
+			g = open(get_target_file_path(path), 'w')
+			g.write(new_content)
+			g.close()
+			return len(buf)
+		elif False:
+			None
+			#files created from nothing 
+		else:
+			raise "error"		
 
 	def ftruncate(self, path, size, fh=None):
 		return -errno.EOPNOTSUPP#
